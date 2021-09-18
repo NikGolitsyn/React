e5b7f1eea51ef3bddcfda4bb7bff6fb6a4c1cf34
NikGolitsyn <78685778+NikGolitsyn@users.noreply.github.com>
2021-09-19 00:58:37 +0300

    Delete lesson1/task4 directory

--- lesson1/task4/review_build/static/js/main.626733fd.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptask4=this.webpackJsonptask4||[]).push([[0],{4:function(e,t,s){"use strict";s.r(t);s(1);var c=s(3),n=s.n(c),r=(s(9),s(0)),a=document.querySelector("#root"),i=Object(r.jsxs)("div",{className:"greeting",children:[Object(r.jsx)("h1",{className:"greeting__title",children:"Hello, world!"}),Object(r.jsx)("p",{className:"greeting__text",children:"I'm learning React"})]});n.a.render(i,a)},9:function(e,t,s){}},[[4,1,2]]]);
-//# sourceMappingURL=main.626733fd.chunk.js.map
