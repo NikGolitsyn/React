7715c329bdbd5ebe756adb2e207182f1bd1951cd
Nikita <myk.holitsyn@gmail.com>
2021-09-21 02:31:31 +0300

    all

--- lesson3/hw2/src/Profile.jsx
+++ lesson3/hw2/src/Profile.jsx
@@ -4,12 +4,10 @@ import dateOfBirthday from './dateOfBirthday';
 export default props => {
   return (
     <>
-      <p className="profile__name">
-        {props.userData.firstName} {props.userData.lastName}
-      </p>
-      <p className="profile__birth">
-        Was born {dateOfBirthday(props.userData.birthDate)} in {props.userData.birthPlace}
-      </p>
+      <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
+      <div className="profile__birth">
+        {`Was born ${dateOfBirthday(props.userData.birthDate)} in ${props.userData.birthPlace}`}
+      </div>
     </>
   );
 };
