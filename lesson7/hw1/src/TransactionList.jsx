436e80098fcdf3655f43b7d57b3d121f11e10dd3
Nikita <myk.holitsyn@gmail.com>
2021-09-27 15:25:49 +0300

    all

---
+++ lesson7/hw1/src/TransactionList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Transaction from './Transaction';
+
+const TransactionList = ({ transactions }) => {
+  return (
+    <ul className="transactions">
+      {transactions.map(transaction => (
+        <Transaction key={transaction.id} {...transaction} />
+      ))}
+    </ul>
+  );
+};
+
+export default TransactionList;
