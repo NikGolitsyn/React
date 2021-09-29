2366f914fe59559190a3101b3342926d5be1252c
Nikita <myk.holitsyn@gmail.com>
2021-09-29 19:11:29 +0300

    all

---
+++ lesson9/hw1/src/Filter.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Filter extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { count, filterText, onChange } = this.props;
+    return (
+      <div className="filter">
+        <span className="filter__count">{count}</span>
+        <input type="text" className="filter__input" value={filterText} onChange={onChange} />
+      </div>
+    );
+  }
+}
+
+export default Filter;
---
+++ lesson9/hw1/src/UserList.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import User from './User';
+import Filter from './Filter';
+
+class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+    };
+  }
+
+  filterHandler = event => {
+    this.setState({
+      filterText: event.target.value,
+    });
+  };
+
+  render() {
+    const { filterText } = this.state;
+    const userList = [...this.props.users].filter(user =>
+      user.name.toLowerCase().includes(filterText.toLowerCase()),
+    );
+    return (
+      <div>
+        <Filter filterText={filterText} count={userList.length} onChange={this.filterHandler} />
+        <ul className="users">
+          {userList.map(user => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default UserList;
