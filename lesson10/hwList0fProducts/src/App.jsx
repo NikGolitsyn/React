59c702fb5ca1322bb8e3b5cbe3ee66e0718eb228
Nikita <myk.holitsyn@gmail.com>
2021-10-01 20:33:49 +0300

    all

---
+++ lesson10/hwList0fProducts/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import FilterableProductTable from './FilterableProductTable';
+
+const App = ({ products }) => {
+  return <FilterableProductTable products={products} />;
+};
+
+export default App;
