bcf46af14bd9a45235284956b43b5c69dff83521
Nikita <myk.holitsyn@gmail.com>
2021-10-06 00:26:08 +0300

    all

--- lesson13/ReactRouter/src/User.jsx
+++ lesson13/ReactRouter/src/User.jsx
@@ -1,60 +1,26 @@
 import React from 'react';
-import { getUser } from './userGateway';
+import PropTypes from 'prop-types';
 
-class User extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userData: null,
-    };
+const User = ({ userData }) => {
+  if (!userData) {
+    return null;
   }
 
-  changeHandler = e => {
-    if (!e.target.closest('li')) {
-      return;
-    }
-    this.fetchUser();
-  };
+  const { avatar_url, name, location } = userData;
 
-  fetchUser = () => {
-    getUser(this.props.match.params.userId).then(body => {
-      this.setState({
-        userData: body,
-      });
-    });
-  };
-
-  shouldComponentUpdate(nextProps) {
-    return nextProps.match.params.userId === this.props.match.params.userId;
-  }
-
-  componentDidMount() {
-    window.addEventListener('click', this.changeHandler);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('click', this.changeHandler);
-  }
-
-  render() {
-    const { userData } = this.state;
-
-    if (!userData) {
-      return null;
-    }
-    console.log(this.props.match.params.userId);
-    const { avatar_url, location, name } = userData;
-
-    return (
-      <div className="user">
-        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
-        <div className="user__info">
-          <span className="user__name">{name}</span>
-          <span className="user__location">{location}</span>
-        </div>
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default User;
+
+User.propTypes = {
+  userData: PropTypes.object.isRequired,
+};
--- lesson13/ReactRouter/src/Users.jsx
+++ lesson13/ReactRouter/src/Users.jsx
@@ -1,28 +1,48 @@
 import React from 'react';
-import { Route, Link, Switch, useRouteMatch } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import User from './User';
+import { getUser } from './userGateway';
 
-const Users = () => {
-  const { url } = useRouteMatch();
-  return (
-    <div className="page__content">
-      <h1>Users</h1>
-      <ul className="navigation">
-        <li className="navigation__item">
-          <Link to={`${url}/github`}>Github</Link>
-        </li>
-        <li className="navigation__item">
-          <Link to={`${url}/facebook`}>Facebook</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route exact path={url}>
-          <span>Select a user please</span>
-        </Route>
-        <Route path={`${url}/:userId`} component={User} />
-      </Switch>
-    </div>
-  );
-};
+class Users extends React.Component {
+  state = {
+    userData: null,
+  };
+
+  fetchUser = id => {
+    getUser(id).then(body => {
+      this.setState({
+        userData: body,
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div className="page__content">
+        <h1>Users</h1>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/users/github" onClick={() => this.fetchUser('github')}>
+              Github
+            </Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/users/facebook" onClick={() => this.fetchUser('facebook')}>
+              Facebook
+            </Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/users">
+            <span>Select a user please</span>
+          </Route>
+          <Route path="/users/:userId">
+            <User userData={this.state.userData} />
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
+}
 
 export default Users;
