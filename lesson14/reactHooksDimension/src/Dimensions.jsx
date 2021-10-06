0ca1814fa7c42c7cbf8613f8442de692bca18115
Nikita <myk.holitsyn@gmail.com>
2021-10-06 17:58:00 +0300

    all

---
+++ lesson14/reactHooksDimension/src/Dimensions.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+const Dimensions = () => {
+  const [dimensions, setDimensions] = useState({ width: null, height: null });
+
+  useEffect(() => {
+    setDimensions({ width: window.innerWidth, height: window.innerHeight });
+
+    const onResize = e => {
+      const { innerHeight, innerWidth } = e.target;
+      setDimensions({ width: innerWidth, height: innerHeight });
+    };
+
+    window.addEventListener('resize', onResize);
+
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
+
+  const { width, height } = dimensions;
+
+  return (
+    <div className="dimensions">
+      {width}px - {height}px
+    </div>
+  );
+};
+
+export default Dimensions;
