eab7d0a526ec5a3995665d3293bcd4bad8ed3c8a
Nikita <myk.holitsyn@gmail.com>
2021-11-26 17:26:25 +0200

    additional practice

--- practice/useCallback/src/App.jsx
+++ practice/useCallback/src/App.jsx
@@ -11,7 +11,10 @@ const App = () => {
 
   // useCallback как и useMemo оборачивает объект но полностью его возвращает generateItemsFromAPI это не результат ф-и а сама функция
   // useCallback нужен для того чтобы функция не изменялась при новом рендере, чтобы она кэшировалась(сохранялась), чтобы не было лишних её вызовов
-  const generateItemsFromAPI = useCallback((indexNumber) => new Array(count).fill('').map((_, index) => `Element ${index + indexNumber}`), [count]);
+  const generateItemsFromAPI = useCallback(
+    indexNumber => new Array(count).fill('').map((_, index) => `Element ${index + indexNumber}`),
+    [count],
+  );
 
   return (
     <div>
---
+++ typeScript-practice/types/types.js
@@ -0,0 +1,23 @@
+var str = 'Hello';
+console.log(str);
+var isFetching = true;
+var isLoading = false;
+var int = 42;
+var float = 4.2;
+var num = 3e10;
+var message = 'Hello Typescript';
+var numberArray = [1, 1, 2, 3, 5, 8, 13];
+var numberArray2 = [1, 2, 3, 4, 5, 6];
+var words = ['Hello', 'Typescript'];
+// Tuple
+var contact = ['name', 123456];
+// Any
+var variable = 42;
+// ...
+variable = 'New string';
+variable = [];
+// ===
+function sayMyName(name) {
+    console.log(name);
+}
+sayMyName('John');
